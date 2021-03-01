59320796dea7389ae4b000ca781aca0ab30387ee
devansh5 <devkumar5436@gmail.com>
2021-03-01 19:29:41 +0530

    rename folder backend & frontend

--- server/backend/backend/settings.py
+++ server/backend/backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fatmugapp',
+    'api',
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken'
